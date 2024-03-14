6720e65a29c2a120d72f2a662f98efef92348954
ElovefishW <145687596+ElovefishW@users.noreply.github.com>
2024-03-13 21:01:44 -0400

    Update dim_product.sql

--- include/dbt/models/transform/dim_product.sql
+++ include/dbt/models/transform/dim_product.sql
@@ -1,6 +1,4 @@
--- dim_product.sql
--- StockCode isn't unique, a product with the same id can have different and prices
--- Create the dimension table
+-- Create the dimension table for the stock code and make sure there will be no null value for the price 
 SELECT DISTINCT
     {{ dbt_utils.generate_surrogate_key(['StockCode', 'Description', 'UnitPrice']) }} as product_id,
 		StockCode AS stock_code,
