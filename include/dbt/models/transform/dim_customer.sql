46641f8f121ddf40c39a99696bac9f0f6bb4f8b7
ElovefishW <145687596+ElovefishW@users.noreply.github.com>
2024-03-13 20:58:17 -0400

    Update dim_customer.sql

--- include/dbt/models/transform/dim_customer.sql
+++ include/dbt/models/transform/dim_customer.sql
@@ -1,6 +1,4 @@
--- dim_customer.sql
-
--- Create the dimension table
+-- Now we do the dimension table transformation to make sure the country table is combined with the customer table based on the country name.
 WITH customer_cte AS (
 	SELECT DISTINCT
 	    {{ dbt_utils.generate_surrogate_key(['CustomerID', 'Country']) }} as customer_id,
